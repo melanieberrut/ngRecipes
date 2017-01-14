cd154525564b7d8907e62b872044e0c8deecddc4
Melanie Berrut <melanieberrut.dev@gmail.com>
2017-01-14 20:14:13 +0000

    Added query parameters to API to limit the data ex: /api/recipes?offset=5&count=1

--- api/controllers/recipes.controllers.js
+++ api/controllers/recipes.controllers.js
@@ -3,9 +3,34 @@ var recipeData = require('../data/recipe-data.json');
 // Get all recipes
 module.exports.recipesGetAll = function(req, res){
 	console.log('Get the recipes');
+	console.log(req.query);
+
+	// Set default values for query
+	var offset = 0;
+	var count = 5;
+
+	// extract the value
+	// > Check if the query exist in the request
+	// > and check that the query offset exist as well
+	if( req.query && req.query.offset ) {
+		// if both exist, offset param exist and define it as offset value
+		// convert the string to a number
+		offset = parseInt(req.query.offset, 10);
+	}
+	// > Check if the query exist in the request
+	// > and check that the count offset exist as well
+	if( req.query && req.query.count ) {
+		// if both exist, offset param exist and define it as offset value
+		// convert the string to a number
+		count = parseInt(req.query.count, 10);
+	}
+
+	// take the data, slide it with the offset (starting point) and offset+count (end point)
+	var returnData = recipeData.slice(offset, offset+count);
+
 	res
 	.status(200)
-	.json( recipeData );
+	.json( returnData );
 };
 
 // Get a specific recipe
