b01c3cb9796386d28e1be56490940ac6ed3390ea
Melanie Berrut <melanieberrut.dev@gmail.com>
2017-01-15 16:27:51 +0000

    Finished schema for recipes, created subsets, stored geolocation

--- api/data/recipes.model.js
+++ api/data/recipes.model.js
@@ -1,8 +1,39 @@
 var mongoose = require('mongoose');
 
 // Build a basic schema
-//
-// name: path | String: type
+
+// Nested schema for reviews (to be defined before the parent schema)
+var reviewSchema = new mongoose.Schema({
+	name : {
+		type: String,
+		required: true
+	},
+	stars: {
+		type: Number,
+		min: 0,
+		max: 5,
+		required: true
+	},
+	review: {
+		type: String,
+		required: true
+	},
+	createdOn: {
+		type: Date,
+		default: Date.now
+	}
+});
+
+// Nested schema for rooms (to be defined before the parent schema)
+var roomsSchema = new mongoose.Schema({
+	type : String,
+	number : Number,
+	description : String,
+	photos : [String],
+	price: Number
+});
+
+// Parent schema
 var recipeSchema = new mongoose.Schema({
 	// name: String
 	// stars: Number,
@@ -19,8 +50,22 @@ var recipeSchema = new mongoose.Schema({
 	services: [String],
 	description: String,
 	photos: [String],
-	currency: String
+	currency: String,
+	reviews: [reviewSchema], // parsing the name of the review schema inside the recipe schema
+	rooms: [roomsSchema],
+	location: {
+		address: String,
+		// longitude (E/W) first, latitude (N/S) second
+		coordinates: {
+			type: Number,
+			index: '2dsphere'// to use for location based search
+		},
+
+	}
+
 });
 
+// Compiling
+//
 // mongoose.model( nameOfModel, schema, mongoDBcollection);
 mongoose.model('Recipe', recipeSchema, 'recipes');
